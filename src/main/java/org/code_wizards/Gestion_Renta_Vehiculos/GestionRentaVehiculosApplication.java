b52d535165cd68659bbc8ca4a478fdba6a0c5893
SyntaxJeferson <jyaxon-2024442@kinal.edu.gt>
2025-09-01 12:00:53 -0600

    Primer Commit: Se creo el proyecto de la Gestion de Renta de Vehiculos

---
+++ src/main/java/org/code_wizards/Gestion_Renta_Vehiculos/GestionRentaVehiculosApplication.java
@@ -0,0 +1,13 @@
+package org.code_wizards.Gestion_Renta_Vehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRentaVehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionRentaVehiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/code_wizards/Gestion_Renta_Vehiculos/GestionRentaVehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.code_wizards.Gestion_Renta_Vehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRentaVehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
