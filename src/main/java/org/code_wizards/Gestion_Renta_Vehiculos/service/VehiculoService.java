16a5421b95d6f337930c063e9272fbaee491187b
Mcho2021304 <mcho-2021304@kinal.edu.gt>
2025-09-06 14:40:06 -0600

    Se agrego VehiculoService para la funcion del proyecto

--- src/main/java/org/code_wizards/Gestion_Renta_Vehiculos/service/VehiculoService.java
+++ src/main/java/org/code_wizards/Gestion_Renta_Vehiculos/service/VehiculoService.java
@@ -1,4 +1,31 @@
 package org.code_wizards.Gestion_Renta_Vehiculos.service;
 
+import org.code_wizards.Gestion_Renta_Vehiculos.entity.Vehiculo;
+import org.code_wizards.Gestion_Renta_Vehiculos.repository.VehiculoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class VehiculoService {
+
+    @Autowired
+    private VehiculoRepository vehiculoRepository;
+
+    public List<Vehiculo> obtenerTodos() {
+        return vehiculoRepository.findAll();
+    }
+
+    public Vehiculo guardar(Vehiculo vehiculo) {
+        return vehiculoRepository.save(vehiculo);
+    }
+
+    public Vehiculo buscarPorId(Integer matricula) {
+        return vehiculoRepository.findById(matricula).orElse(null);
+    }
+
+    public void eliminar(Integer matricula) {
+        vehiculoRepository.deleteById(matricula);
+    }
 }
